c7c2a192f35bf7c34f9bdbd9d64d6f7dc9ddff9d
rajesh dhiman <rajeshdhimanskt@gmail.com>
2019-09-20 15:50:54 +0530

    hide default link

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -82,7 +82,7 @@ export class Navigation extends Component {
                 ))}
               </div>
             </div>
-            <NavLink to="/default/">Default</NavLink>
+            {/* <NavLink to="/default/">Default</NavLink> */}
             <NavLink to="/contact/">Contact</NavLink>
           </div>
           <button
