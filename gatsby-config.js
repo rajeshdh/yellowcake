9b74469c49149de6eaf579806a999a78f983fb62
rajesh dhiman <rajesh.dhiman@srijan.net>
2020-05-19 16:26:37 +0530

    removed analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,7 +12,7 @@ module.exports = {
       resolve: 'gatsby-plugin-google-tagmanager',
       options: {
         /*id: 'GTM-add_your_tag_here',*/
-        id: 'GTM-W8NFZMK',
+        id: 'GTM-add_your_tag_here',
         includeInDevelopment: false
       }
     },
